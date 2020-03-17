989c46e90842aecf683b184d16d695f9269e6ec6
krkirian <kruzhkovll@gmail.com>
2020-03-17 16:35:42 +0300

    попап формы готовы

--- src/modules/popupForm.js
+++ src/modules/popupForm.js
@@ -1,62 +1,69 @@
 const popupForm = () => {
 
-    const form1 = document.getElementById('free_visit_form');
-    const form2 = document.getElementById('callback_form');
+    const form1 = document.getElementById('form2');
+    const form2 = document.getElementById('form1');
     const forms = [form1, form2];
-    const thanksWindow = document.getElementById('thanks');
-    const statusMessage = document.createElement('div');
 
-    console.log(forms);
+    
+    forms.forEach((elem) => {
 
-    const clearInput = () => {
-        statusMessage.textContent = '';
-    }
+        let popup = elem.parentNode.parentNode.parentNode;
+        let popupClose = () => {
+            popup.style.display = 'none';
+        }
+        const clearInput = () => {
+            statusMessage.textContent = '';
+            const checkbox = elem.querySelector('input[type=checkbox]')
+            checkbox.checked = false;
+            popupClose();
+        }
 
+        let statusMessage = elem.querySelector('h4');
 
-//     form.addEventListener('submit', (event) => {
-//             event.preventDefault();
-//             const formData = new FormData(form);
-//             let body = {};
-//             let formValues = form.querySelectorAll('input');
-//             statusMessage.textContent = 'Загрузка';
-//             form.appendChild(statusMessage);
+        elem.addEventListener('submit', (event) => {
+            event.preventDefault();
+            statusMessage.textContent = 'Загрузка';
+            const formData = new FormData(elem);
+            let body = {};
+            let formValues = elem.querySelectorAll('input');
 
-//             formData.forEach((value, key) => {
-//                 body[key] = value;
-//             });
-//             postData(body)
-//             .then((response) => {
-//                 if(response.status !== 200) {
-//                     throw new Error('status network not 200');
-//                 }   
-//                 console.log(response);
-//                 thanksWindow.style.display = 'block';
-//                 setTimeout(clearInput, 1000);
-//                 for (let i = 0; i < formValues.length; i++) {
-//                     formValues[i].value = '';
-//                 };
-//             })
-//             .catch((error) => {
-//                 statusMessage.textContent = 'Ошибка';
-//                 setTimeout(clearInput, 3000);
-//                 console.error(error);
-
+            formData.forEach((value, key) => {
+                body[key] = value;
+            });
+            postData(body)
+            .then((response) => {
+                if(response.status !== 200) {
+                    throw new Error('status network not 200');
+                }   
+                console.log(response);
+                statusMessage.textContent = 'Мы скоро с вами связжемся!';
+                for (let i = 0; i < formValues.length; i++) {
+                    formValues[i].value = '';
+                };
+                setTimeout(clearInput, 3000);
+            })
+            .catch((error) => {
+                statusMessage.textContent = 'Что-то пошло не так...';
+                console.error(error);  
+            });
                 
-//             });     
+            
+        });
 
-//     });
+    });
 
-//     const postData = (body) => {
-//         return fetch('./server.php', {
-//             method: 'POST',
-//             headers: {
-//                 'Content-type': 'application/json'
-//             },
-//             body: JSON.stringify(body)
-//         });
+
+    const postData = (body) => {
+        return fetch('./server.php', {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify(body)
+        });
         
 
-//     };
+    };
 
    };
 

879a05c19d09c06d1517bc16766b12c79b2ab7f3
krkirian <kruzhkovll@gmail.com>
2020-03-18 17:50:57 +0300

    карусель добавлена

--- src/modules/serviceSlider.js
+++ src/modules/serviceSlider.js
@@ -4,15 +4,36 @@ const serviceSlider = () => {
           slide = slider.querySelectorAll('.slide');
 
     let position = 0;
+
+    let slideWidth = slide[0].offsetWidth;
+    let slideHeight = slide[0].offsetHeight;
+    sliderWrapper.style.height = slideHeight;
+    sliderWrapper.style.width = slideWidth*5;
     
         
 
     const arrowLeft = document.getElementById('arrowLeftService');
+    const arrowRight = document.getElementById('arrowRightService');
 
+    arrowRight.addEventListener('click', () => {
+        position = position - (slideWidth + 10);
+        console.log(position);
+        if (position < -1100) {
+            position = 0;
+            slider.style.transform = `translateX(${position}px)`;
+        } else {
+            slider.style.transform = `translateX(${position}px)`;
+        }
+    });
     arrowLeft.addEventListener('click', () => {
-        position = position - 200;
-        if ( position < -1100)
-        slider.style.left = position + 'px';
+        position = position + (slideWidth + 10);
+        console.log(position);
+        if (position > 0) {
+            position = -1100;
+            slider.style.transform = `translateX(${position}px)`;
+        } else {
+            slider.style.transform = `translateX(${position}px)`;
+        }
     });
 
 }

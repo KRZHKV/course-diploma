1c4d0db4bf6507c2930059a1055ff48135ecde3f
krkirian <kruzhkovll@gmail.com>
2020-03-19 11:35:29 +0300

    исправление калькулятора

--- src/modules/calculator.js
+++ src/modules/calculator.js
@@ -9,70 +9,33 @@ const calculator = () => {
           priceInput = document.getElementById('price-total'),
           promo = form.querySelectorAll('input[name="name"]')[0];
 
-        form.addEventListener('change', () => {
-            if (oneMonth.checked && mozaikaBtn.checked) {
-                if (promo.value === 'ТЕЛО2019'){
-                    let price = 1999;
-                    priceInput.textContent = `${Math.round(+price * 0.7)}`;
-                } else {
-                    priceInput.textContent = '1999';
-                }
-            } else if (sixMonth.checked && mozaikaBtn.checked) {
-                if (promo.value === 'ТЕЛО2019'){
-                    let price = 9999;
-                    priceInput.textContent = `${Math.round(+price * 0.7)}`;
-                } else {
-                    priceInput.textContent = '9990';
-                }
-            } else if (nineMonth.checked && mozaikaBtn.checked) {
-                if (promo.value === 'ТЕЛО2019'){
-                    let price = 13990;
-                    priceInput.textContent = `${Math.round(+price * 0.7)}`;
-                } else {
-                    priceInput.textContent = '13990';
-                }
-            } else if (twelveMonth.checked && mozaikaBtn.checked) {
-                if (promo.value === 'ТЕЛО2019'){
-                    let price = 19990;
-                    priceInput.textContent = `${Math.round(+price * 0.7)}`;
-                } else {
-                    priceInput.textContent = '19990';
-                }
-            } else if (oneMonth.checked && schelkovoBtn.checked) {
-                if (promo.value === 'ТЕЛО2019'){
-                    let price = 2990;
-                    priceInput.textContent = `${Math.round(+price * 0.7)}`;
-                } else {
-                    priceInput.textContent = '2990';
-                }
-            } else if (sixMonth.checked && schelkovoBtn.checked) {
-                if (promo.value === 'ТЕЛО2019'){
-                    let price = 14990;
-                    priceInput.textContent = `${Math.round(+price * 0.7)}`;
-                } else {
-                    priceInput.textContent = '14990';
-                }
-            } else if (nineMonth.checked && schelkovoBtn.checked) {
-                if (promo.value === 'ТЕЛО2019'){
-                    let price = 21990;
-                    priceInput.textContent = `${Math.round(+price * 0.7)}`;
-                } else {
-                    priceInput.textContent = '21990';
-                }
-            } else if (twelveMonth.checked && schelkovoBtn.checked) {
-                if (promo.value === 'ТЕЛО2019'){
-                    let price = 24990;
-                    priceInput.textContent = `${Math.round(+price * 0.7)}`;
-                } else {
-                    priceInput.textContent = '24990';
-                }
-            };
+    let price;
+
+    form.addEventListener('change', () => {
+        if (oneMonth.checked && mozaikaBtn.checked) {
+            price = 1999;
+        } else if (sixMonth.checked && mozaikaBtn.checked) {
+            price = 9999;
+        } else if (nineMonth.checked && mozaikaBtn.checked) {
+            price = 13990;
+        } else if (twelveMonth.checked && mozaikaBtn.checked) {
+            price = 19990;
+        } else if (oneMonth.checked && schelkovoBtn.checked) {
+             price = 2990;
+        } else if (sixMonth.checked && schelkovoBtn.checked) {
+            price = 14990;
+        } else if (nineMonth.checked && schelkovoBtn.checked) {
+            price = 21990;
+        } else if (twelveMonth.checked && schelkovoBtn.checked) {
+            price = 24990;
+        };
+            
+        if (promo.value === 'ТЕЛО2019') {
+            priceInput.textContent = Math.round(price * 0.7);
+        } else {
+            priceInput.textContent = price;
+        };
     });
-    
-
-    
-    
-
 
 };
 
